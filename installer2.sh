5d83cf0f384c59c3a0999cbde920908d12c21523
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-07-30 21:09:50 +0200

    Add files via upload

---
+++ installer2.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#wget -q "--no-check-certificate" https://raw.githubusercontent.com/tar1971/ipaudio/main/installer2.sh -O - | /bin/sh
+######### Only These two lines to edit with new version ######
+version=6.6
+description="Player Update"
+#############################################################
+
+wait
+
+wget "https://raw.githubusercontent.com/tar1971/ipaudio/main/ipaudio-6.6.tar"
+
+tar -xzf ipaudio-6.6.tar.gz  -C /
+
+wait
+
+rm -f /tmp/ipaudio-6.6.tar.gz
+
+echo "   UPLOADED BY  >>>>   TAREK_TT "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0
