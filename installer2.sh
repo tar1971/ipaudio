c83bdf5c8244cfccc52c682b1f08d07af8a83aa1
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-07-30 21:18:46 +0200

    Update installer2.sh

--- installer2.sh
+++ installer2.sh
@@ -9,11 +9,15 @@ wait
 
 wget "https://raw.githubusercontent.com/tar1971/ipaudio/main/ipaudio-6.6.tar"
 
+wait
+
 tar -xzf ipaudio-6.6.tar.gz  -C /
 
 wait
 
 rm -f /tmp/ipaudio-6.6.tar.gz
+
+wait
 
 echo "   UPLOADED BY  >>>>   TAREK_TT "   
 sleep 4;                                                                                                                  
